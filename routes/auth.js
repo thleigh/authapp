429c16d4dea702c53bbf4acbab0942f5dc4cb4ae
Tanner Leigh <tannerhleigh@gmail.com>
2020-08-13 14:15:51 -0700

    [auth] create auth method

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../models');
 
 router.get('/signup', (req, res) => {
   res.render('auth/signup');
@@ -8,5 +9,35 @@ router.get('/signup', (req, res) => {
 router.get('/login', (req, res) => {
   res.render('auth/login');
 });
+
+router.post('/signup', (req, res) => {
+  console.log(req.body);
+  db.user.findOrCreate({
+    where: {
+      email: req.body.email
+    },
+    default: { 
+      name: req.body.name,
+      password: req.body.password
+    }
+  })
+  .then(([user, created])=> {
+    if(created) {
+      //if created, success and redirect to home
+      console.log(`${user.name} was created`);
+      res.redirect('/');
+    } else {
+      //Email already exists
+      console.log('Email already exists')
+      res.redirect('/auth/signup')
+    }
+  })
+  .catch(error => {
+    console.log('Error', error);
+    res.redirect('/auth/signup');
+  })
+})
+
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const layouts = require('express-ejs-layouts');
 const app = express();
+const session = require('express-session');
+const SECRET_SESSION = process.env.SECRET_SESSION;
 
 app.set('view engine', 'ejs');
 
@@ -9,6 +11,17 @@ app.use(require('morgan')('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 app.use(layouts);
+
+//secret: what we actually giving the client to use our site
+//resave: save the sessino even if it's modified, make this false
+//saveUninitialized: if we have a new session, we'll save it, therefore, 
+// setting this to true
+
+app.use(session ({
+  secret: SECRET_SESSION,
+  resave: false,
+  saveUninitialized: true,
+}));
 
 app.get('/', (req, res) => {
   res.render('index');
