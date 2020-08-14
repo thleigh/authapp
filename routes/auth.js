f9ddb6285df8189e93168e98e82b50dd078ec2c5
Tanner Leigh <tannerhleigh@gmail.com>
2020-08-14 11:58:03 -0700

    [alerts] fix alerts

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../models');
-const passport = require('../config/ppConfig')
+const passport = require('../config/ppConfig');
 
 router.get('/signup', (req, res) => {
   res.render('auth/signup');
@@ -24,42 +24,41 @@ router.post('/signup', (req, res) => {
     if (created) {
       // if created, success and redirect to home
       console.log(`${user.name} was created`);
+      // FLASH MESSAGE
       passport.authenticate('local', {
-        //flash message
         successRedirect: '/',
         successFlash: 'Account created and logging in'
-      }) (req, res);
-      res.redirect('/');
+      })(req, res);
+      // before passport authenicate
+      // res.redirect('/');
     } else {
       // Email already exist
       console.log('Email already exist');
-      req.flash('Email already exists. Please try again')
+      // FLASH
+      req.flash('error', 'Email already exist. Please try again.');
       res.redirect('/auth/signup');
     }
   })
   .catch(error => {
     console.log('Error', error);
-    req.flash(`Error, unfortunately... ${error}`)
+    req.flash('error', `Error, unfortunately... ${error}`);
     res.redirect('/auth/signup');
   });
 });
 
-//flash message
+// FLASH MESSAGE
 router.post('/login', passport.authenticate('local', {
   successRedirect: '/',
   failureRedirect: '/auth/login',
-  successFlash: 'Welcome back',
-  failureFlash: 'Either password or email is incorrect. Please try again'
+  successFlash: 'Welcome back.',
+  failureFlash: 'Either email or password is incorrect. Please try again.'
 }));
-
-router.post('/login', (req, res) => {
-
-})
 
 router.get('/logout', (req, res) => {
   req.logOut();
-  req.flash('See you soon. Logging out.')
+  // FLASH MESSAGE
+  req.flash('success', 'See you soon. Logging out.');
   res.redirect('/');
-})
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const app = express();
 const session = require('express-session');
 const SECRET_SESSION = process.env.SECRET_SESSION;
 const passport = require('./config/ppConfig');
-const flash = require('connect-flash')
+const flash = require('connect-flash');
 
-const { use } = require('chai');
-
+// require the authorization middleware at the top of the page
+const isLoggedIn = require('./middleware/isLoggedIn');
 
 app.set('view engine', 'ejs');
 
@@ -16,42 +16,42 @@ app.use(require('morgan')('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 app.use(layouts);
-//flash for temporary messages to the user
-app.use(flash())
 
-//middleware to have our message accessable for ever view
-app.use((req, res, next) => {
-  //before every request, we will attach our current user to res.local
-  res.locals.alerts = req.flash();
-  res.locals.currentUser = req.user;
-  next();
-})
-
-//secret: what we actually giving the client to use our site
-//resave: save the sessino even if it's modified, make this false
-//saveUninitialized: if we have a new session, we'll save it, therefore, 
+// secret: What we actually giving the user to use our site / session cookie
+// resave: Save the session even if it's modified, make this false
+// saveUninitialized: if we have a new session, we'll save it, therefore,
 // setting this to true
 
-app.use(session ({
+app.use(session({
   secret: SECRET_SESSION,
   resave: false,
-  saveUninitialized: true,
+  saveUninitialized: true
 }));
 
-//initialize passport and run sessions as middleware
+// Initialize passport and run session as middleware
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.get('/', (req, res) => {
-  res.render('index', {alert: req.flash});
+// flash for temporary messages to the user
+app.use(flash());
+
+// middleware to have our message accessible for every view
+app.use((req, res, next) => {
+  // before every route, we will attached our current user to res.local
+  res.locals.alerts = req.flash();
+  res.locals.currentUser = req.user;
+  next();
 });
 
-app.get('/profile', (req, res) => {
+app.get('/', (req, res) => {
+  res.render('index', {alerts: res.locals.alerts});
+});
+
+app.get('/profile', isLoggedIn, (req, res) => {
   res.render('profile');
 });
 
 app.use('/auth', require('./routes/auth'));
-
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
